ca01053c080eced05436b07402f413c8c5ceed24
Marvin <santamariamarvin1@gmail.com>
2021-11-28 09:27:24 -0500

    Primeros archivos

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ejemplolambda
+{
+    public class Program
+    {
+        private delegate double Mathematical(double value);
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Started");
+            Console.WriteLine();
+
+            Mathematical doublePower = (double value) => { return Math.Pow(value, 2); };
+            Mathematical halfValue = (double value) => { return value / 2; };
+
+            Console.WriteLine(doublePower(3));
+            Console.WriteLine(halfValue(3));
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
